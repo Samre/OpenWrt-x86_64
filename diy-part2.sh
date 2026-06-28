2c80df3ed2a658a993b02da8c6da9c8cb5672c82
Samre <1728527686@qq.com>
2026-06-28 19:53:48 +0800

    fix: patch shortcut-fe for Linux 6.18+ API changes

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,15 @@ sed -i 's/192.168.1.1/192.168.216.10/g' package/base-files/files/bin/config_gene
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+# Patch shortcut-fe for Linux 6.18+ compatibility
+# from_timer() and del_timer_sync() were removed in Linux 6.18
+SHORTCUT_DIR="package/qca/shortcut-fe/shortcut-fe/src"
+if [ -d "$SHORTCUT_DIR" ]; then
+  # Replace from_timer() with container_of()
+  sed -i 's/from_timer(si, tl, timer)/container_of(tl, struct sfe_ipv4, timer)/g' "$SHORTCUT_DIR/sfe_ipv4.c"
+  sed -i 's/from_timer(si, tl, timer)/container_of(tl, struct sfe_ipv6, timer)/g' "$SHORTCUT_DIR/sfe_ipv6.c"
+  # Replace del_timer_sync() with timer_delete_sync()
+  sed -i 's/del_timer_sync(\&si->timer)/timer_delete_sync(\&si->timer)/g' "$SHORTCUT_DIR/sfe_ipv4.c"
+  sed -i 's/del_timer_sync(\&si->timer)/timer_delete_sync(\&si->timer)/g' "$SHORTCUT_DIR/sfe_ipv6.c"
+  echo "shortcut-fe patched for Linux 6.18+"
+fi
